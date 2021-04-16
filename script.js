a77175dafc4be41c81f9405243f81376681196f6
tikenov <azamatkz90@gmail.com>
2021-04-16 14:06:52 +0600

    Second Part is finished

--- script.js
+++ script.js
@@ -1,31 +1,80 @@
-const { position, paths } = data;
-
+const { position, paths, list } = data;
+const clickSound = document.getElementById("click-sound");
+const icons = document.querySelectorAll(".icon");
+const squares = document.querySelectorAll(".square");
 const forward = document.getElementById("btn-forward");
 const backward = document.getElementById("btn-backward");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const ratingBtn = document.getElementById("rating");
 const map = document.getElementById("map");
 const model = document.getElementById("model");
+const modal = document.getElementById("modal");
 const stepDuration = 500;
 
-let current = 0;
-if (localStorage.getItem('current'))
-    current = localStorage.getItem('current');
+let currentListItem = 0;
+let currentStep = 0;
+if (localStorage.getItem('currentStep'))
+    currentStep = localStorage.getItem('currentStep');
 
 model.style.transform = "translate(-50%, -100%)";
-model.style.left = `${position[current][0]}px`;
-model.style.top = `${position[current][1]}px`;
+model.style.left = `${position[currentStep][0]}px`;
+model.style.top = `${position[currentStep][1]}px`;
 
+icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        clickSound.play();
+    });
+});
 
+left.addEventListener('click', () => {
+    if (currentListItem == list.length - 1) return;
+    resetSquares();
+    currentListItem++;
+    console.log(currentListItem);
+    setSquares();
+});
+right.addEventListener('click', () => {
+    if (currentListItem == -7) return;
+    resetSquares();
+    currentListItem--;
+    console.log(currentListItem);
+    setSquares();
+});
 
+// List contains numbers: 1-with user logo,  2-with add button
+const setSquares = () => {
+    squares.forEach((square, i) => {
+        if (list[i + currentListItem] > 0) {
+            const node = document.createElement('div');
+            node.className = "user";
+            square.appendChild(node)
+        }
+        if (list[i + currentListItem] == 2) {
+            const node = document.createElement('div');
+            node.className = "add";
+            square.appendChild(node)
+        }
+    });
+}
+
+const resetSquares = () => {
+    squares.forEach(square => {
+        square.innerHTML = "";
+    })
+}
 
 const enableButtons = () => {
     forward.disabled = false
     backward.disabled = false
 }
+
 const disableButtons = (timeout) => {
     forward.disabled = true
     backward.disabled = true
     setTimeout(() => enableButtons(false), timeout);
 }
+
 const resetModel = () => {
     model.className = "";
     map.removeChild(model);
@@ -33,22 +82,26 @@ const resetModel = () => {
 }
 
 forward.addEventListener('click', () => {
-    if (current == paths.length) return;
+    if (currentStep == paths.length) return;
     resetModel();
-    model.setAttribute("style", `offset-path: path('${paths[current++]}')`);
+    model.setAttribute("style", `offset-path: path('${paths[currentStep++]}')`);
     model.style.animationDuration = stepDuration + "ms";
     disableButtons(stepDuration);
-    localStorage.setItem('current', current);
+    localStorage.setItem('currentStep', currentStep);
 })
 
 backward.addEventListener('click', function () {
-    if (current == 0) return;
+    if (currentStep == 0) return;
     resetModel();
-    model.setAttribute("style", `offset-path: path('${paths[--current]}')`);
+    model.setAttribute("style", `offset-path: path('${paths[--currentStep]}')`);
     model.style.animationDirection = 'reverse';
     model.style.animationDuration = stepDuration + "ms";
-    localStorage.setItem('current', current);
+    localStorage.setItem('currentStep', currentStep);
     disableButtons(stepDuration);
 })
 
+ratingBtn.addEventListener('click', function () {
+    modal.classList.add('visible');
+});
+setSquares();
 window.onload = () => document.getElementById('loader').style.display = 'none';
